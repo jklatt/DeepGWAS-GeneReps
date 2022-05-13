7fb68fe73e3009ab09e6031eb3941bccf68682d0
Szx <ShelleyShu@S-SHU-2.local>
2022-05-13 14:21:44 +0200

    two SNP positions

--- toy_gwas_loader.py
+++ toy_gwas_loader.py
@@ -17,7 +17,7 @@ train=True
 num_in_train=2
 
 
-def generate_samples(gene_length_mean,gene_length_var, target_mutation_val, target_mutation_pos,num_genes,train=True):
+def generate_samples(gene_length_mean,gene_length_var, target_mutation_val, target_mutation_pos, num_genes,train=True):
     # generate some toy sample of GWAS data with integers
     
     gene_length=np.int(np.random.normal(gene_length_mean,gene_length_var,1))
@@ -36,6 +36,7 @@ def generate_samples(gene_length_mean,gene_length_var, target_mutation_val, targ
                     single_label=values==target_mutation_val
                 else:
                     single_label= False
+
 
                 if 3>i>0 or i>8:
                     encoding_region=1
@@ -72,6 +73,7 @@ def generate_samples(gene_length_mean,gene_length_var, target_mutation_val, targ
 
             for i in range(0,gene_length):
                 values = randint(0, 4)
+
                 if i==target_mutation_pos:
                     single_label=values==target_mutation_val
                 else:
@@ -111,6 +113,110 @@ def generate_samples(gene_length_mean,gene_length_var, target_mutation_val, targ
 #     print(genes)
 #     labels=data[1]
 #     print(labels)
+
+def generate_samples_twoSNPs(gene_length_mean,gene_length_var, target_mutation_val1, target_mutation_pos1,target_mutation_val2, target_mutation_pos2, num_genes,train=True):
+    # generate some toy sample of GWAS data with integers
+    
+    gene_length=np.int(np.random.normal(gene_length_mean,gene_length_var,1))
+    if train==True:
+        data_list=[]*num_genes
+        label_list=[]
+        bag_label_list=[]
+        for k in range(0,num_genes):
+            data=[[]]*gene_length
+            label=[]
+            seed(num_genes+k)
+
+            for i in range(0,gene_length):
+                values = randint(0, 4)
+                if i==target_mutation_pos1:
+                    single_label = values==target_mutation_val1 
+                    
+                elif i==target_mutation_pos2:
+                    single_label = values==target_mutation_val2 
+
+                else:
+                    single_label= False
+
+                if 3>i>0 or i>8:
+                    encoding_region=1
+                else: 
+                    encoding_region=0
+
+                item=[i,values,encoding_region]
+                item=np.expand_dims(item,axis=0)           
+                label.append(single_label)
+                data[i]=item
+     
+            if True in label:
+                bag_label=[True]
+                
+            else:
+                bag_label=[False]
+                
+
+            data_list.append(data)
+            label_list.append(label)
+            bag_label_list.append(bag_label)
+            
+
+    elif train==False:
+        data_list=[]*num_genes
+        label_list=[]
+        bag_label_list=[]
+
+        for k in range(0,num_genes):
+
+            data=[[]]*gene_length
+            label=[]
+            seed(num_genes+10000+k)
+
+            for i in range(0,gene_length):
+                values = randint(0, 4)
+                if i==target_mutation_pos1:
+                    single_label = values==target_mutation_val1 
+                    
+                elif i==target_mutation_pos2:
+                    single_label = values==target_mutation_val2 
+
+                else:
+                    single_label= False
+
+                if 3>i>0 or i>8:
+                    encoding_region=1
+                else: 
+                    encoding_region=0
+
+                item=[i,values,encoding_region] 
+                item=np.expand_dims(item,axis=0)             
+                label.append(single_label)
+                data[i]=item
+
+            if True in label:
+                bag_label=True
+               
+            else:
+                bag_label=False
+
+            data_list.append(data)
+            label_list.append(label)
+            bag_label_list.append(bag_label)
+    
+
+    return data_list, bag_label_list, label_list
+
+# data_list, bag_label_list, label_list=generate_samples_twoSNPs(gene_length_mean=13, gene_length_var=4,target_mutation_val1=0,target_mutation_pos1=3,target_mutation_val2=3,target_mutation_pos2=7,num_genes=200)
+
+# train_data=TensorDataset(torch.tensor(data_list),torch.tensor(bag_label_list),torch.tensor(label_list))
+# train_loader =DataLoader(train_data,batch_size=num_in_train, shuffle=False)
+       
+                           
+# for _, data in enumerate(train_loader):
+#     genes=data[0]
+#     print(genes)
+#     labels=data[1]
+#     print(labels)
+
 
 
 
